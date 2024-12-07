387e902c9c7199c4361fe31f24e2e02daa5217cb
Sarah <sarahcordobasalgado@gmail.com>
2024-12-07 00:23:48 -0500

    dios por favor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
         View::composer('*', function ($view) {
             $menuItems = [
                 ['name' => 'Inicio', 'url' => '/', 'icon' => 'bi bi-house-door'],
